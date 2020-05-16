eb78a2e323a6512a89af5729047d4f3d31e8d257
Ismael <ismael.perez@tajamar365.com>
2020-05-16 17:06:24 +0200

    Añadidos el repositorio que acede a las api  al proyecto xamarin

---
+++ ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/Models/LogInModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClienteApiClinica.Models
+{
+    public class LogInModel
+    {
+        public String NombreUsuario { get; set; }
+        public String Contrasena { get; set; }
+    }
+}
