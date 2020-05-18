5e465c1f07852a77826944af273ee4047c0d9f30
Ismael <ismael.perez@tajamar365.com>
2020-05-19 01:06:58 +0200

    Refactorizado

--- ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/Views/Chat/ChatView.xaml.cs
+++ ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/Views/Chat/ChatView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ClienteApiClinica.Models;
+using ClienteApiClinica.VIewModels.Chat;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,83 +15,18 @@ namespace ClienteApiClinica.Views.Chat
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChatView : ContentPage
     {
-        public ChatView()
+        public ChatView(String username)
         {
             InitializeComponent();
 
-            //When a new meessage is added the scroll is move to the las message
-            this.Chat.ChildAdded += async (object sender, ElementEventArgs e) =>
+            ChatModelView bindingcontext = (this.BindingContext as ChatModelView);
+            bindingcontext.TargetUserName = username;
+
+            bindingcontext.Messages.CollectionChanged += ((object sender, NotifyCollectionChangedEventArgs e) =>
             {
-                //esto no funciona por lo que se calcula mediante la altura del chat  
-                //this.ChatScroll.ScrollToAsync(e.Element, ScrollToPosition.Start, false);
-
-                //Alguien me explica porque no hay un evento que sea despues de insertar el hijo¿?
-                //This method needs to wait to the child to be aded to the chat so the calculation of the height of the stack layout is ok
-                await Task.Delay(10);
-                this.ChatScroll.ScrollToAsync(0, this.Chat.Height, false);
-            };
-
-            this.btnMessageInput.Clicked += (Object sender, EventArgs e) =>
-            {
-                String msg = this.MessageInput.Text;
-
-                if (msg != "")
-                {
-                    this.AddMyMessage(msg);
-
-                    //Enviar msg a signalR
-                    this.MessageInput.Text = "";
-                }
-
-            };
-
-        }
-
-        private void AddRemoteMessage(String msg) 
-        {
-            Frame FrameMsg = GenerateMessage(msg, LayoutOptions.Start, Color.LightGreen);
-            this.Chat.Children.Add(FrameMsg);
-        }
-        private void AddMyMessage(String msg) 
-        {
-            Frame FrameMsg = GenerateMessage(msg, LayoutOptions.End, Color.White);
-            this.Chat.Children.Add(FrameMsg);
-        }
-
-        private Frame  GenerateMessage(string text, LayoutOptions position,Color backgroundColor)
-        {
-            Frame msgFrame = new Frame()
-            {
-                CornerRadius = 10,
-                Padding = new Thickness(10, 5, 15, 5),
-                HorizontalOptions = position,
-                BackgroundColor = backgroundColor,
-            };
-
-            StackLayout msgRow = new StackLayout()
-            {
-                Orientation = StackOrientation.Horizontal,
-            };
-
-            msgRow.Children.Add(new Label()
-            {
-                Text = text,
-                FontSize = 20,
-                TextColor = Color.DarkSlateGray
-
+                Mensaje msg = (this.BindingContext as ChatModelView).Messages.Last();
+                chatList.ScrollTo(msg, ScrollToPosition.End, false);
             });
-
-            msgRow.Children.Add(new Label()
-            {
-                Text = DateTime.Now.ToString("HH:mm"),
-                FontSize = 10,
-                TextColor = Color.DarkSlateGray,
-                VerticalOptions = LayoutOptions.End
-            });
-
-            msgFrame.Content = msgRow;
-
-            return msgFrame;
         }
     }
 }
