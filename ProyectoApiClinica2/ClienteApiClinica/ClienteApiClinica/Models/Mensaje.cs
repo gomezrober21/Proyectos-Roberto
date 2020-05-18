2de5df6663cd67f69f0b2da15b6b90ade2a95a86
Ismael <ismael.perez@tajamar365.com>
2020-05-19 01:05:57 +0200

    Refactorizado de la parte de chat para cumplir con los standares correctos

---
+++ ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/Models/Mensaje.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClienteApiClinica.Models
+{
+    public class Mensaje
+    {
+        public String Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool IsRemote { get; set; }
+
+    }
+}
