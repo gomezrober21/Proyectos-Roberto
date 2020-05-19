e1a5185d2d2464a4fa514f28d6b145a7db9bd0bc
Ismael <ismael.perez@tajamar365.com>
2020-05-19 10:38:13 +0200

    Creada la vista de peticion de citas

---
+++ ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/ViewModels/CitaFisioModelView.cs
@@ -0,0 +1,33 @@
+﻿using ClienteApiClinica.Models;
+using ClienteApiClinica.Repositories;
+using ClienteApiClinica.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ClienteApiClinica.ViewModels
+{
+    public class CitaFisioModelView:ViewModelBase
+    {
+        public ConsultaFisio Cita { get; set; }
+        RepositoryClinica RepositoryClinica;
+
+        public Command InsertCita
+        {
+            get
+            {
+                return new Command(() => {
+                    RepositoryClinica.CogerCitaFisio(
+                        id: Cita.Id,
+                        nombre: Cita.Nombre,
+                        apellido: Cita.Apellido,
+                        fecha: Cita.Fecha,
+                        edad: Cita.Edad,
+                        direccion: Cita.Direccion,
+                        telefono: Cita.Telefono);
+                });
+            }
+        }
+    }
+}
