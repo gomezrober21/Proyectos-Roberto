e4a5108f6d3187c1af97931e8e79f4b748e51ed9
Ben <bensei09@gmail.com>
2020-05-19 13:12:04 +0200

    Menu y Registro

---
+++ ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/VIewModels/RegistroViewModel.cs
@@ -0,0 +1,43 @@
+﻿using FisioXamarin.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FisioXamarin.ViewModels
+{
+ public  class RegistroViewModel
+    {
+        ApiService _apiServices = new ApiService();  
+        public String Email { get; set; }
+        public String Password { get; set; }       
+        public String NombreUsuario { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public int Edad { get; set; }
+        public int Telefono { get; set; }
+        public String Mensaje { get; set; }
+        public ICommand ComandoRegistro
+        {
+            get
+            {
+                return new Command(async ()  =>
+                {
+                  var isSuccess =   _apiServices.RegisterAsync(Email, Password, NombreUsuario, Nombre, Apellido, Edad, Telefono);
+                    if (await isSuccess)
+                    {
+                        Mensaje = "Registro realizado";
+                    }
+
+                    else
+                    {
+                        Mensaje = "Vuelvelo a intentar";
+                    }
+                        
+                });
+            }
+        }
+    }
+}
---
+++ ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/Views/EntrenamientoPersonal.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FisioXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntrenamientoPersonal : ContentPage
+    {
+        public EntrenamientoPersonal()
+        {
+            InitializeComponent();
+        }
+    }
+}
