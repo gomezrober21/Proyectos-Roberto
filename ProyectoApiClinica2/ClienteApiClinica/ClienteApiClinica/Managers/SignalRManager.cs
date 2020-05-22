b5e626cd2cb3a824956261946d4803323f9e9dd9
Ismael <ismael.perez@tajamar365.com>
2020-05-22 12:58:25 +0200

    Implementado el codigo para el evento de mensaje recibido

--- ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/Managers/SignalRManager.cs
+++ ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/Managers/SignalRManager.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace ClienteApiClinica.Managers
@@ -13,12 +14,12 @@ namespace ClienteApiClinica.Managers
         Dictionary<String, List<Mensaje>> ListaMensaje;
         HubConnection connection;
         public string Url { get; }
-        SignalRManager()
+        public SignalRManager()
         {
             this.Url = "https://proyectoapiclinica2.azurewebsites.net" + "/chatHub";
         }
 
-        public void StartConnection()
+        public async void StartConnection()
         {
             connection = new HubConnectionBuilder()
                 .WithUrl(this.Url, options => {
@@ -28,6 +29,28 @@ namespace ClienteApiClinica.Managers
             this.connection.StartAsync().ContinueWith((task) => {
                 if (!task.IsFaulted)
                 {
+                    Debug.WriteLine("SignalR ConnectionStarted");
+
+                    connection.On<string, string>("RecibedFrom", (message,user ) =>
+                    {
+                        if (ListaMensaje[user] == null)
+                        {
+                            ListaMensaje[user] = new List<Mensaje>();
+                        }
+                        ListaMensaje[user]
+                            .Add(new Mensaje() 
+                            { 
+                                IsRemote = true,
+                                Message  =message,
+                                TimeStamp = DateTime.Now
+                            });
+                    });
+
+                    //Implementacion global sin hacer
+                    //connection.On<string, string>("ReceiveGloval", (message) =>
+                    //{
+
+                    //});
 
                 }
                 else
@@ -43,23 +66,36 @@ namespace ClienteApiClinica.Managers
             if (this.connection.State == HubConnectionState.Connected)
             {
                 this.connection.SendAsync("SendMessageToUser", msg, targetUserName);
-            }else
+
+                if (ListaMensaje[targetUserName] == null)
+                {
+                    ListaMensaje[targetUserName] = new List<Mensaje>();
+                }
+                ListaMensaje[targetUserName]
+                    .Add(new Mensaje()
+                    {
+                        IsRemote = false,
+                        Message = msg,
+                        TimeStamp = DateTime.Now
+                    });
+            }
+            else
             {
                 throw new Exception("Connection not started. Current state "+ this.connection.State);
             }
         }
 
-        public void SendMessageGlobal(string msg)
-        {
-            if (this.connection.State == HubConnectionState.Connected)
-            {
-                this.connection.SendAsync("SendGlobalMessage", msg);
-            }
-            else
-            {
-                throw new Exception("Connection not started. Current state " + this.connection.State);
-            }
-        }
+        //public void SendMessageGlobal(string msg)
+        //{
+        //    if (this.connection.State == HubConnectionState.Connected)
+        //    {
+        //        this.connection.SendAsync("SendGlobalMessage", msg);
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("Connection not started. Current state " + this.connection.State);
+        //    }
+        //}
 
     }
 }
