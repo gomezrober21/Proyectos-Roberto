d71dadc0c0d4379230721e001a5920bce4121f7b
Ismael <ismael.perez@tajamar365.com>
2020-05-25 09:59:53 +0200

    Añadida injeccion de dependencias

---
+++ ProyectoApiClinica2/ClienteApiClinica/ClienteApiClinica/DependencyInjection/DependencyDeclarations.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using ClienteApiClinica.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClienteApiClinica.DependencyInjection
+{
+    static class DependencyDeclarations
+    {
+
+        public static IContainer BuildContainer()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterType<RepositoryClinica>();
+
+            return builder.Build();
+
+        }
+
+    }
+}
